0855646557f9f78c44fab0add5bf10eeba8d94f0
Alex Benko <benkoalex521@gmail.com>
2021-05-25 19:32:06 -0400

    minor

--- server/scripts/uploadToImugr.ts
+++ server/scripts/uploadToImugr.ts
@@ -20,17 +20,17 @@ const uploadToImugr = async () => {
     const albums = await fsAsync.readdir(photoFolderPath)
     for (const album of albums) {
       if (album.includes('.')) continue
+      console.log('Currently Uploading Album : ', album)
 
       await db('INSERT INTO ALBUMS(album_name) VALUES($1)', [album])
       const albumId = await db('SELECT id FROM albums WHERE album_name = $1', [album])
 
       const fullAlbumPath = `${photoFolderPath}/${album}`
       const imagesInAlbum = await fsAsync.readdir(fullAlbumPath)
+
       // imugr api requires absolute file paths
       const toUpload = imagesInAlbum.filter(img => img !== '.DS_Store').map(img => fullAlbumPath + '/' + img)
       const responses = await client.upload(toUpload)
-      console.log(responses)
-      // break
 
       for (const i in responses) {
         await db('INSERT INTO images(album_id,img_url) VALUES($1,$2)', [albumId, responses[i].data.link])
