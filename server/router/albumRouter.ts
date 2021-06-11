003243783d15e55c4ce9b2ca40946e0be1b985e9
Alex Benko <benkoalex521@gmail.com>
2021-06-10 23:04:20 -0400

    /all route for album info

--- server/router/albumRouter.ts
+++ server/router/albumRouter.ts
@@ -3,11 +3,12 @@ import db from '../postgresql/db'
 import log from '../logging/log'
 
 const router = express.Router()
-// child route of /api/album
+// child route of /api/albums
 router.get('/ping', (req, res) => res.send('Pong'))
 
 router.get('/all', async (req, res) => {
-  // const albumsInfo = await db('SELECT * FROM albums;')
+  const albumsInfo = await db('SELECT * FROM albums;')
+  res.send(albumsInfo)
 })
 
 export default router
