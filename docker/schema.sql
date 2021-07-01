8fd773484156339c052b05584150f0d53db4bfba
Alex Benko <benkoalex521@gmail.com>
2021-06-30 22:34:24 -0400

    moved all docker related files to same dir

---
+++ docker/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS albums CASCADE;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE albums(
+  id SERIAL primary key,
+  album_name VARCHAR(255) NOT NULL,
+  album_desc VARCHAR(560) DEFAULT NULL,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE images(
+  img_id SERIAL primary key,
+  album_id INT,
+  img_name VARCHAR(255) DEFAULT NULL,
+  img_desc VARCHAR(560) DEFAULT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now(),
+  FOREIGN KEY(album_id)
+    REFERENCES albums(id)
+      ON DELETE SET NULL
+);
+
+CREATE TABLE users(
+  userid SERIAL primary key,
+  fname VARCHAR(60) NOT NULL,
+  lname VARCHAR(60) NOT NUll,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  pass VARCHAR(255) NOT NULL
+);
