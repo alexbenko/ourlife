690243d3743adac4f6c36206a05b987e0603e9f8
Alex Benko <benkoalex521@gmail.com>
2021-07-01 13:20:03 -0400

    init script for docker

--- docker/init.sql
+++ docker/init.sql
@@ -1,3 +1,37 @@
 CREATE USER photo;
 CREATE DATABASE photobooth;
 GRANT ALL PRIVILEGES ON DATABASE photobooth TO photo;
+
+DROP TABLE IF EXISTS albums CASCADE;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS users;
+
+\c photobooth photo;
+CREATE TABLE albums(
+  id SERIAL primary key,
+  album_name VARCHAR(255) NOT NULL,
+  album_desc VARCHAR(560) DEFAULT NULL,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE images(
+  img_id SERIAL primary key,
+  album_id INT,
+  img_name VARCHAR(255) DEFAULT NULL,
+  img_desc VARCHAR(560) DEFAULT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now(),
+  FOREIGN KEY(album_id)
+    REFERENCES albums(id)
+      ON DELETE SET NULL
+);
+
+CREATE TABLE users(
+  userid SERIAL primary key,
+  fname VARCHAR(60) NOT NULL,
+  lname VARCHAR(60) NOT NUll,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  pass VARCHAR(255) NOT NULL
+);
