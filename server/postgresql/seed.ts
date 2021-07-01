4359de03335320884115634131c16b6b4ed976ec
Alex Benko <benkoalex521@gmail.com>
2021-07-01 13:24:58 -0400

    Removed try/catch block

--- server/postgresql/seed.ts
+++ server/postgresql/seed.ts
@@ -28,35 +28,29 @@ const formatAlbumName = (text:string) => {
 }
 
 const saveFilePaths = async () => {
-  try {
-    const photoFolderPath = path.join(__dirname, '../static/photos')
-    const albums = await fsAsync.readdir(photoFolderPath)
+  const photoFolderPath = path.join(__dirname, '../static/photos')
+  const albums = await fsAsync.readdir(photoFolderPath)
 
-    // eslint-disable-next-line array-callback-return
-    const result = await Promise.all(albums.map(async (album) => {
-      if (album.includes('.')) return false // these are directories and should not include any files
+  // eslint-disable-next-line array-callback-return
+  const result = await Promise.all(albums.map(async (album) => {
+    if (album.includes('.')) return false // these are directories and should not include any files
 
-      const albumName = formatAlbumName(album)
-      await db('INSERT INTO ALBUMS(album_name) VALUES($1);', [albumName])
-      const albumId = await db('SELECT id FROM albums WHERE album_name = $1;', [albumName])
-      const { id } = albumId[0]
+    const albumName = formatAlbumName(album)
+    await db('INSERT INTO ALBUMS(album_name) VALUES($1);', [albumName])
+    const albumId = await db('SELECT id FROM albums WHERE album_name = $1;', [albumName])
+    const { id } = albumId[0]
 
-      const fullAlbumPath = `${photoFolderPath}/${album}`
-      const imagesInAlbum = await fsAsync.readdir(fullAlbumPath)
-      const imagePaths = imagesInAlbum.filter(img => img !== '.DS_Store').map(img => fullAlbumPath + '/' + img)
+    const fullAlbumPath = `${photoFolderPath}/${album}`
+    const imagesInAlbum = await fsAsync.readdir(fullAlbumPath)
+    const imagePaths = imagesInAlbum.filter(img => img !== '.DS_Store').map(img => fullAlbumPath + '/' + img)
 
-      for (const imageUrl of imagePaths) {
-        await db('INSERT INTO IMAGES(album_id, img_url) VALUES($1,$2);', [id, imageUrl.split('/photos')[1]])
-      }
-      return true
-    }))
-    console.log(result)
-    console.log('Successfully Saved All Image File Paths')
-    process.exit(1)
-  } catch (err) {
-    console.log(err.message)
-    process.exit(1)
-  }
+    for (const imageUrl of imagePaths) {
+      await db('INSERT INTO IMAGES(album_id, img_url) VALUES($1,$2);', [id, imageUrl.split('/photos')[1]])
+    }
+    return true
+  }))
+  console.log(result)
+  console.log('Successfully Saved All Image File Paths')
 }
 
 export default saveFilePaths
