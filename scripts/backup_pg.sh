c605b8cbde4fb063825457b1cc402a4a78ba4316
Alex Benko <benkoalex521@gmail.com>
2021-07-15 12:01:14 -0400

    Updated paths and specified user in backup script

--- scripts/backup_pg.sh
+++ scripts/backup_pg.sh
@@ -2,7 +2,7 @@
 if [ -f .env ]; then
   # load environment variables into bash script
   export $(cat .env)
-  pg_dump $PG_DB > db_backup.bak
+  pg_dump $PG_DB -U PG_USER > /backups/db_backup.bak
 else
   echo "No .env file."
 fi
