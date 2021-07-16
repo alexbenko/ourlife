41b7677acd575cc2ca2a649b95f53a2c85049168
Alex Benko <benkoalex521@gmail.com>
2021-07-16 09:04:16 -0400

    compressing function rotates image

--- src/lib/compress.ts
+++ src/lib/compress.ts
@@ -1,5 +1,9 @@
 import sharp from 'sharp'
 
+// sharp throws away metadata when compressing, causing a lot compressed images to be roated
+// calling rotate ensures the image is in the original orientation without the need of keeping the metadata keeping the image size smaller
+// see : https://stackoverflow.com/questions/48716266/sharp-image-library-rotates-image-when-resizing
+
 export default async function (oldImagePath : string, outputPath : string) {
-  await sharp(oldImagePath).webp({ quality: 30, force: true }).toFile(outputPath)
+  await sharp(oldImagePath).rotate().webp({ quality: 30, force: true }).toFile(outputPath)
 }
