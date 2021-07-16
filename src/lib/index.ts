14764aa199f08c0f127896dbbaac9d0c6b210e21
Alex Benko <benkoalex521@gmail.com>
2021-07-16 08:40:28 -0400

    imported new reusable functions into module

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,10 +2,15 @@ import fsAsync from './fsAsync'
 import log from './log'
 import randomString from './randomString'
 import sendEmail from './sendEmail'
+import compress from './compress'
+import dateStamp from './dateStamp'
 
+// I do my best to follow the DRY method while coding, these are resuable functions that I use through this project
 export {
   fsAsync,
   log,
   randomString,
-  sendEmail
+  sendEmail,
+  compress,
+  dateStamp
 }
