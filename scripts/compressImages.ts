312f744732cd9da208af99d4a1bf282394b34941
Alex Benko <benkoalex521@gmail.com>
2021-07-16 09:04:35 -0400

    script that compresses all of my static photos

---
+++ scripts/compressImages.ts
@@ -0,0 +1,27 @@
+import { fsAsync, compress } from '../src/lib'
+import path from 'path'
+
+// await sharp('IMG_3861.jpeg').webp({ quality: 30 }).toFile('a.webp')
+const compressImages = async () => {
+  const imagesDir = path.join(__dirname, '../static/old')
+  const outDir = path.join(__dirname, '../static/photos') // this is where the new compressed images go
+  const images = await fsAsync.readdir(imagesDir)
+
+  for (const dir of images) {
+    if (dir === '.DS_Store') continue
+    await fsAsync.mkdir(path.join(outDir, dir))
+    const rootDir = path.join(imagesDir, dir)
+    const images = await fsAsync.readdir(rootDir)
+    const outDirAlbum = path.join(outDir, dir)
+    console.log('Compressing Album: ', dir)
+    for (let i = 0; i < images.length; i++) {
+      console.log(Math.floor((i / images.length) * 100), '%')
+      if (images[i] === '.DS_Store') continue
+      const oldImagePath = path.join(rootDir, images[i])
+      const newImagePath = path.join(outDirAlbum, `${dir}-${i}.webp`)
+      await compress(oldImagePath, newImagePath)
+    }
+  }
+}
+
+compressImages()
