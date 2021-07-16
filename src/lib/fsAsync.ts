252c08272e5953038f2b98dc089c35133132ec9d
Alex Benko <benkoalex521@gmail.com>
2021-07-16 09:05:26 -0400

    Promisified mkdir

--- src/lib/fsAsync.ts
+++ src/lib/fsAsync.ts
@@ -4,5 +4,6 @@ import { promisify } from 'util'
 // promify used fs moduels since they dont return promises
 export default {
   readdir: promisify(fs.readdir),
-  readFile: promisify(fs.readFile)
+  readFile: promisify(fs.readFile),
+  mkdir: promisify(fs.mkdir)
 }
