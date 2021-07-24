a6229c492423714a6e4af8602dd2d4a007eac980
Alex Benko <benkoalex521@gmail.com>
2021-07-24 08:14:59 -0400

    Starting the admin router which has a unqiue root endpoint every time the server starts

---
+++ src/router/adminRouter.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express'
+import { log, dateStamp } from '../lib'
+import path from 'path'
+
+// each time the server is started, this router is assigned a random string for the root path
+const router = express.Router()
+
+router.get('/status', (req : Request, res: Response) => res.status(200).send('Everythin is good'))
+router.get('/error/today', async (req : Request, res: Response) => {
+  try {
+    const errorLogToday = path.join(__dirname, `../logs/errors/${dateStamp(new Date())}.log`)
+    res.status(200).sendFile(errorLogToday)
+  } catch (err) {
+    console.log(err)
+    log.error(`Error retrieving recent error log: ${err}`)
+    res.status(404)
+  }
+})
+
+export default router
