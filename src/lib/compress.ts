a245cc474b74551bb98bc4b95eb1ff64ad5b7f91
Alex Benko <benkoalex521@gmail.com>
2021-07-30 22:25:03 -0400

    Accepts a buffer now

--- src/lib/compress.ts
+++ src/lib/compress.ts
@@ -4,6 +4,6 @@ import sharp from 'sharp'
 // calling rotate ensures the image is in the original orientation without the need of keeping the metadata keeping the image size smaller
 // see : https://stackoverflow.com/questions/48716266/sharp-image-library-rotates-image-when-resizing
 
-export default async function (oldImagePath : string, outputPath : string) {
-  await sharp(oldImagePath).rotate().webp({ quality: 30, force: true }).toFile(outputPath)
+export default async function (oldImage : string | Buffer, outputPath : string) {
+  await sharp(oldImage).rotate().webp({ quality: 50, force: true }).toFile(outputPath)
 }
