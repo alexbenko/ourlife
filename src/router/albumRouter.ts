ab1b2143b2b08de208886623fd998edc1b0267ff
Alex Benko <benkoalex521@gmail.com>
2021-08-01 12:28:14 -0400

    updated query

--- src/router/albumRouter.ts
+++ src/router/albumRouter.ts
@@ -4,17 +4,16 @@ import { log } from '../lib'
 
 const router = express.Router()
 // child route of /api/albums
-router.get('/ping', (req, res) => res.send('Pong'))
-
 router.get('/all', async (req, res) => {
-  const albumsInfo = await db('SELECT * FROM albums;')
+  const albumsInfo = await db('SELECT id,displayname FROM albums;')
   res.send(albumsInfo)
 })
 
 router.get('/:albumId', async (req, res) => {
   try {
     const { albumId } = req.params
-    const album = await db('SELECT * FROM images WHERE album_id = $1', [albumId])
+    const album = await db('SELECT * FROM images WHERE albumid = $1', [albumId])
+
     res.send(album)
   } catch (err) {
     log.error(`Error Retrieving Albumid:${req.params.albumId}: ${err}`)
