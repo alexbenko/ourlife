bd4dcb355dae2c1d0c4ad857f29a3583fd41ebe8
Alex Benko <benkoalex521@gmail.com>
2021-08-04 10:29:22 -0400

    Data for photo gallery component is formatted in backend now

--- src/router/albumRouter.ts
+++ src/router/albumRouter.ts
@@ -10,10 +10,20 @@ router.get('/all', async (req, res) => {
 })
 
 router.get('/:albumId', async (req, res) => {
+  // this formats the data for the photo gallery component on the front end since at most can be 4 columns
+  const formatData = (imageData) => {
+    // i want an array with 4 sub arrays of similar length
+    const formatted = []
+    for (let i = 4; i > 0; i--) {
+      console.log(i)
+      formatted.push(imageData.splice(0, Math.ceil(imageData.length / i)))
+    }
+    return formatted
+  }
   try {
     const { albumId } = req.params
-    const album = await db('SELECT * FROM images WHERE albumid = $1', [albumId])
-
+    let album = await db('SELECT * FROM images WHERE albumid = $1', [albumId])
+    album = formatData(album)
     res.send(album)
   } catch (err) {
     log.error(`Error Retrieving Albumid:${req.params.albumId}: ${err}`)
