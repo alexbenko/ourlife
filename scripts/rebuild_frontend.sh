fd14e07f66cc5131ababe1a8b6bfdbf3dc5e6670
Alex Benko <benkoalex521@gmail.com>
2021-08-04 10:31:30 -0400

    webhook to rebuild front end

---
+++ scripts/rebuild_frontend.sh
@@ -0,0 +1,5 @@
+# the front end is deployed on vercel and is built with Nextjs
+# The front end is statically built and any time i make changes in the backend i will most likely need to rebuild the front end
+# vercel gives you webhooks that will rebuild the app
+
+curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_gJYM9lwpelerXhS5l3myBCnkiLIK/yEpFOX5h5m
