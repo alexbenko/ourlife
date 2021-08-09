3bee6c022afe832a3419720a9b75a56751d12df5
Alex Benko <benkoalex521@gmail.com>
2021-08-09 15:58:20 -0400

    Checks jwt

---
+++ src/middleware/checkToken.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import redisHelpers from '../redis/redisHelpers'
+import { log, randomString } from '../lib'
+
+export default async function authenticateToken (req, res : Response, next : NextFunction) {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token === null) return res.sendStatus(401)
+  let tokenSecret = await redisHelpers.get('tokenSecret')
+
+  if (!tokenSecret) {
+    tokenSecret = randomString(128)
+    await redisHelpers.set('tokenSecret', tokenSecret, 86400)
+  }
+
+  jwt.verify(token, tokenSecret as string, (err: any, user: any) => {
+    log.error(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
