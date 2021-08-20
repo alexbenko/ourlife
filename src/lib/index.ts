8f8361cd8d2cc4f382a93e1d7f7e72110cd1c91f
Alex Benko <benkoalex521@gmail.com>
2021-08-20 06:42:45 -0400

    imported token

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ import log from './log'
 import randomString from './randomString'
 import compress from './compress'
 import dateStamp from './dateStamp'
+import token from './token'
 
 // I do my best to follow the DRY method while coding, these are resuable functions that I use through this project
 export {
@@ -10,5 +11,6 @@ export {
   log,
   randomString,
   compress,
-  dateStamp
+  dateStamp,
+  token
 }
