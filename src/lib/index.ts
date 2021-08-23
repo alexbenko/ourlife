ca0c65e798e9c5368deb9258ca156ebc4f48552a
Alex Benko <benkoalex521@gmail.com>
2021-08-23 09:00:19 -0400

    Helps determine if string is empty

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@ import randomString from './randomString'
 import compress from './compress'
 import dateStamp from './dateStamp'
 import token from './token'
+import isEmptyString from './isEmptyString'
 
 // I do my best to follow the DRY method while coding, these are resuable functions that I use through this project
 export {
@@ -12,5 +13,6 @@ export {
   randomString,
   compress,
   dateStamp,
-  token
+  token,
+  isEmptyString
 }
---
+++ src/lib/isEmptyString.ts
@@ -0,0 +1,3 @@
+export default function isEmptyString (word : string) : boolean {
+  return (word === '' || word.length < 1)
+}
