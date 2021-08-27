876fb6f0d3f8d03d4cba129309df3392ae569e12
Alex Benko <benkoalex521@gmail.com>
2021-08-26 21:45:55 -0500

    created authentication function as well as a middleware and removed retrieving secret from redis for now

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,44 +1,61 @@
 import jwt from 'jsonwebtoken'
-import redisHelpers from '../redis/redisHelpers'
+// import redisHelpers from '../redis/redisHelpers'
 import { Request, Response, NextFunction } from 'express'
-import { log, randomString } from '.'
+import { log } from '.'
+require('dotenv').config()
+const TOKEN_SECRET = process.env.TOKEN_SECRET
 
-const authenticateToken = async function (req, res : Response, next : NextFunction) {
+const authenticateTokenMiddleware = async function (req : Request, res : Response, next : NextFunction) {
   const authHeader = req.headers.authorization
   const token = authHeader && authHeader.split(' ')[1]
 
   if (token === null) return res.sendStatus(401)
+  /*
   let tokenSecret = await redisHelpers.get('tokenSecret')
 
   if (!tokenSecret) {
     tokenSecret = randomString(128)
     await redisHelpers.set('tokenSecret', tokenSecret, 86400)
   }
-
-  jwt.verify(token, tokenSecret as string, (err: any, user: any) => {
-    log.error(err)
+  */
+  jwt.verify(token, TOKEN_SECRET as string, (err) => {
+    // log.error(err as string)
 
     if (err) return res.sendStatus(403).send('Invalid Token. Please Login Again')
-
-    req.user = user
 
     next()
   })
 }
 
-const generateToken = async function (userData : {}) {
+const generateToken = async function (userData = {}, expiration: string = '86400s') {
   // TODO ADD a redis helper that accomplishes this functionality
+  /*
   let tokenSecret = await redisHelpers.get('tokenSecret')
 
   if (!tokenSecret) {
     tokenSecret = randomString(128)
     await redisHelpers.set('tokenSecret', tokenSecret, 86400)
   }
-  return jwt.sign(userData, tokenSecret, { expiresIn: '86400s' })
+  */
+  return jwt.sign(userData, TOKEN_SECRET, { expiresIn: expiration })
+}
+
+const authenticateToken = (token) => {
+  let valid : boolean
+  jwt.verify(token, TOKEN_SECRET as string, (err, decoded) => {
+    if (err || !decoded) {
+      log.error(err, false)
+      valid = false
+    } else {
+      valid = true
+    }
+  })
+  return valid
 }
 
 const token = {
-  authenticateToken,
-  generateToken
+  authenticateTokenMiddleware,
+  generateToken,
+  authenticateToken
 }
 export default token
