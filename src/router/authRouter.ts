fe799878b5517a6596a8f75b448fd010313fa955
Alex Benko <benkoalex521@gmail.com>
2021-08-26 21:44:25 -0500

    moved upload to auth

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from 'express'
+import multer from 'multer'
+import path from 'path'
 
 import { ResponseObj } from '../interfaces/user'
-import { log, isEmptyString, token } from '../lib'
+import { log, isEmptyString, token, compress } from '../lib'
+import db from '../postgresql/db'
 import User from '../classes/User'
 
 const router = Router()
 // child route of /api/auth
 
-router.post('/signup', async (req : Request, res: Response) => {
+router.post('/signup/:token', async (req : Request, res: Response) => {
   let responseObj : ResponseObj
   try {
     const body = { ...req.body }
@@ -58,7 +61,36 @@ router.post('/signup', async (req : Request, res: Response) => {
   }
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', token.authenticateTokenMiddleware, async (req, res) => {
 
 })
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+router.post('/single/:albumId', token.authenticateTokenMiddleware, upload.single('file'), async (req : Request, res: Response) => {
+  try {
+    const { albumId } = req.params
+    const existingAlbum = await db('SELECT id, dirname from albums WHERE id = $1', [albumId])
+    if (!existingAlbum.length) {
+      res.status(201).send('Album Does Not exist')
+    } else {
+      // multer stores the image as a buffer here
+      const { buffer, originalname } = req.file
+
+      const staticFilePath = process.env.NODE_ENV === 'production' ? '../static' : '../../static'
+      const photoFolderPath = path.join(__dirname, staticFilePath)
+      const url = `/photos/${existingAlbum[0].dirname}/${originalname.split('.')[0]}.webp`
+      const newImagePath = photoFolderPath + url
+
+      await compress(buffer, newImagePath)
+      // TODO: Need to insert new image into db
+      res.send({ url })
+    }
+  } catch (err) {
+    log.error(`Error uploading a single Image: ${err}`)
+    res.status(400).send('Failed')
+  }
+})
+
 export default router
