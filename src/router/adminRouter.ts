a3ae710cbdc9f565b10b46bddefae616b5895082
Alex Benko <benkoalex521@gmail.com>
2021-08-26 22:58:10 -0500

    Sends signup email to recipient

--- src/router/adminRouter.ts
+++ src/router/adminRouter.ts
@@ -1,12 +1,14 @@
-import express, { Request, Response } from 'express'
-import { log, dateStamp } from '../lib'
+import { readdir } from 'fs/promises'
 import path from 'path'
+import { render, sendEmail } from '../emails'
+import { Router, Request, Response } from 'express'
+import { log, dateStamp, token } from '../lib'
+require('dotenv').config()
 
 // each time the server is started, this router is assigned a random string for the root path
-const router = express.Router()
+const router = Router()
 
-router.get('/status', (req : Request, res: Response) => res.status(200).send('Everythin is good'))
-router.get('/error/today', async (req : Request, res: Response) => {
+router.get('/errors/today', async (req : Request, res: Response) => {
   try {
     const errorLogToday = path.join(__dirname, `../logs/errors/${dateStamp(new Date())}.log`)
     res.status(200).sendFile(errorLogToday)
@@ -14,6 +16,46 @@ router.get('/error/today', async (req : Request, res: Response) => {
     console.log(err)
     log.error(`Error retrieving recent error log: ${err}`)
     res.status(400).send('Failed To Retrieve File.')
+  }
+})
+
+/*
+router.get('/errors/all', async (req : Request, res: Response) => {
+  try {
+    const errorLogs = await readdir(path.join(__dirname, '../logs/errors'))
+    console.log(errorLogs)
+  } catch (err) {
+    log.error('Error Retrieving all error logs')
+    res.status(400).send('Failed To Retrieve File.')
+  }
+})
+*/
+
+router.get('/sendsignup/:to', async (req : Request, res: Response) => {
+  try {
+    const { to } = req.params
+    if (!to) {
+      return res.status(404).send('Request Failed')
+    }
+    console.log(to)
+    const newToken = await token.generateToken({}, '1800s') // valid for 30 minutes
+
+    const inProduction = process.env.NODE_ENV === 'production'
+    const port = inProduction ? Number(process.env.PORT) : 8080
+    const serverUrl = inProduction ? process.env.PRODUCTION_URL : `http://localhost:${port}`
+    const signupUrl = `${serverUrl}/signup/${newToken}`
+
+    const templatePath = path.join(__dirname, '../../views/signup.hbs')
+
+    const template = await render(templatePath, { signupUrl, expiresIn: '30 Minutes' })
+    await sendEmail('Here is the Signup Url', template, template, to)
+
+    log.info(`Sent Signup Email to ${to}`, true)
+    res.send('Email Sent!')
+  } catch (err) {
+    console.log(err)
+    log.error(`Error Sending Signup Email: ${err}`)
+    res.status(404).send('Error')
   }
 })
 
